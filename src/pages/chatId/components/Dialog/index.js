0d5846a8e319ef6a429f0b64ee146f2af54a5314
Alex-region <alex_yasinsky@mail.com>
2021-09-24 14:52:47 +0300

    24/09/21 task 7: redux-thunk installed and bot's answer added in middleware

--- src/pages/chatId/components/Dialog/index.js
+++ src/pages/chatId/components/Dialog/index.js
@@ -8,7 +8,7 @@ import {useEffect, useState, useMemo } from "react";
 
 import './style.scss';
 import {useSelector, useDispatch } from "react-redux";
-import {addMessage} from "../../../../store/messages/actions";
+import {addMessageWithThunk} from "../../../../store/messages/actions";
 import {getMessages} from "../../../../store/messages/selectors";
 import {getProfileName} from "../../../../store/profile/selectors";
 
@@ -25,7 +25,6 @@ export default function Dialog() {
 
 	const dispatch = useDispatch();
 	const messages = useSelector(getMessages);
-	console.log(messages);
 
 	const messageList = useMemo(() => {
 		return messages[buddyId] || [];
@@ -52,22 +51,8 @@ export default function Dialog() {
 
 
 	function handleSubmit(message) {
-		dispatch(addMessage(buddyId, message));
+		dispatch(addMessageWithThunk(buddyId, message, user, buddyName));
 	}
-
-	useEffect(() => {
-		if (messageList.length !==0 && !(messageList[messageList.length - 1].user === buddyName)) {
-			setTimeout(() => {
-				let message = {
-					isOwner: false,
-					user: buddyName,
-					text: `${messageList[messageList.length - 1].text}?`,
-					time: new Date().toLocaleTimeString()
-				};
-				dispatch(addMessage(buddyId, message));
-			}, 1500);
-		}
-	}, [buddyName, dispatch, messageList, buddyId])
 
 	return (
 		<>
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { chatsReducer } from './chats';
 import { profileReducer } from "./profile";
 import { messagesReducer} from "./messages";
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
 	profile: profileReducer,
@@ -9,5 +10,8 @@ const rootReducer = combineReducers({
 	messages: messagesReducer
 });
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
 export const store = createStore(
-	rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+	rootReducer, composeEnhancers(applyMiddleware(thunk)));
