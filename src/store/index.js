8d0913e2662fbf0410d2535a3171f2f66f8fd605
Alex-region <alex_yasinsky@mail.com>
2021-09-24 17:22:11 +0300

    24/09/21 task 7: redux-persist added

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import { chatsReducer } from './chats';
 import { profileReducer } from "./profile";
 import { messagesReducer} from "./messages";
 import thunk from 'redux-thunk';
+import storage from 'redux-persist/lib/storage/session';
 
 const rootReducer = combineReducers({
 	profile: profileReducer,
@@ -12,6 +14,18 @@ const rootReducer = combineReducers({
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['profile']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 export const store = createStore(
-	rootReducer, composeEnhancers(applyMiddleware(thunk)));
+	persistedReducer, 
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+export const persistor = persistStore(store);
